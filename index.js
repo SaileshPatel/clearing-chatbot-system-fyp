8d822fa5a5bad942c0b2ee0ff955b486e62c5663
Sailesh Patel <saileshapatel@gmail.com>
2019-12-16 16:49:19 +0000

    add course param to entry requirement

--- index.js
+++ index.js
@@ -17,8 +17,22 @@ app.listen(port, () =>{
 })
 
 app.post('/getentryrequirements', (req, res) => {
+    const course = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.course ? req.body.result.parameters.course : ''
+
+    const fulfilText;
+    switch(course) {
+        case "Computer Science":
+            fulfilText = "You will need a BTEC in IT Practitioners"
+            break;
+        case "English":
+            fulfilText = "The grades are A*A*A"
+            break;
+        default:
+            fulfilText = "This course could not be found"
+    }
+
     return res.json({
-        fulfillmentText: "Your requirements are A*A*A*",
+        fulfillmentText: fulfilText,
         source: 'getentryrequirements'
     })
 })
