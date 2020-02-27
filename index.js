81a5c98f4473f25232cfcff76b41ff3dd0eab9d3
Sailesh Patel <saileshapatel@gmail.com>
2020-02-27 15:03:34 +0000

    add example of CSV file format

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
 
 app.set('view engine', 'pug')
 
