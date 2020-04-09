8d0e64413db36cb9cca3c47adf65e130cf422ef7
Sailesh Patel <saileshapatel@gmail.com>
2020-04-10 00:45:19 +0100

    move course query to statement

--- routes/allocate-space-manually.js
+++ routes/allocate-space-manually.js
@@ -2,9 +2,10 @@ const db = require("./../db");
 var express = require("express");
 var router = express.Router();
 
+var allCourses = "SELECT ucas_code, course_name, course_spaces FROM Courses ORDER BY course_name ASC;"
 
 router.get('/', (req, res) => {
-    db.query("SELECT ucas_code, course_name, course_spaces FROM Courses ORDER BY course_name ASC;", [])
+    db.query(allCourses, [])
     .then(response => {
         res.render('add-space-manually', {title: "Allocate Space", courses: response.rows});
     })
@@ -17,7 +18,7 @@ router.post("/", (req, res) => {
     var course_to_update = req['body']['ucas_code'];
     db.query("UPDATE Courses SET course_spaces = course_spaces - 1 WHERE ucas_code = $1 AND course_spaces > 0;", [course_to_update])
         .then(response => {
-            db.query("SELECT ucas_code, course_name, course_spaces FROM Courses ORDER BY course_name ASC;", [])
+            db.query(allCourses, [])
                 .then(response => {
                     res.render('add-space-manually', {title: "Allocate Space", courses: response.rows});
                 })
