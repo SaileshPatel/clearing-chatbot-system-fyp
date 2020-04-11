d5fc6a933dd670632831bd81f5faf7ca84541224
Sailesh Patel <saileshapatel@gmail.com>
2020-04-11 01:29:21 +0100

    move space allocation logic to own function

--- routes/allocate-space-manually.js
+++ routes/allocate-space-manually.js
@@ -2,10 +2,7 @@ const db = require("./../db");
 var express = require("express");
 var router = express.Router();
 
-var allCourses = "SELECT ucas_code, course_name, course_spaces FROM Courses ORDER BY course_name ASC;"
-
 router.get('/', (req, res) => {
-
     getAllCourses()
         .then(response => {
             res.render('add-space-manually', {title: "Allocate Space", courses: response});
@@ -17,19 +14,20 @@ router.get('/', (req, res) => {
 
 router.post("/", (req, res) => {
     var course_to_update = req['body']['ucas_code'];
-    db.query("UPDATE Courses SET course_spaces = course_spaces - 1 WHERE ucas_code = $1 AND course_spaces > 0;", [course_to_update])
+
+    allocateSpace(course_to_update)
         .then(response => {
-            db.query(allCourses, [])
-                .then(response => {
-                    res.render('add-space-manually', {title: "Allocate Space", courses: response.rows});
+            getAllCourses()
+                .then(resp => {
+                    res.render('add-space-manually', {title: "Allocate Space", courses: resp});
                 })
                 .catch(err => {
-                    res.render('add-space-manually', {title: "Allocate Space", fail_message: "There has been a problem with retrieving the list of courses. Please try again later."});
+                    res.render('add-space-manually', {title: "Allocate Space", fail_message: err});
                 })
-            //res.render("index", {title: "Success"});
         })
         .catch(err => {
-            res.render('add-space-manually', {title: "Allocate Space", fail_message: "There has been an issue with adding a space to the course at this time."});
+            res.render('add-space-manually', {title: "Allocate Space", fail_message: err['fail_message']});
+
         })
 })
 
@@ -42,6 +40,20 @@ function getAllCourses(){
             })
             .catch(err => {
                 reject("There has been an issue retrieving courses from our database. If this issue persists, please contact a system administrator.");
+            })
+    })
+}
+
+function allocateSpace(ucas_code){
+    return new Promise(function(resolve, reject){
+        db.query("UPDATE Courses SET course_spaces = course_spaces - 1 WHERE ucas_code = $1 AND course_spaces > 0;", [ucas_code])
+            .then(response => {
+                resolve()
+            })
+            .catch(err => {
+                reject({
+                    fail_message: 'There has been an issue with adding a space to the course at this time'
+                })
             })
     })
 }
