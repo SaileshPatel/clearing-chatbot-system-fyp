3ceae8f4551e1174dc44c3feab5f80e52a731060
Sailesh Patel <saileshapatel@gmail.com>
2020-04-19 01:13:26 +0100

    add new application stage function to create query strings and parameters for each step

--- application/index.js
+++ application/index.js
@@ -1,21 +1,34 @@
 const db = require("./../db");
 
+function applicationStage(stage, request){
+    switch(stage){
+        case 'Application - yes':
+            return {
+                queryString: "INSERT INTO students (first_name, last_name, ucas_code) VALUES ($1, $2, (SELECT ucas_code FROM courses WHERE course_name LIKE $3)) RETURNING student_id;",
+                queryParams: [request.body.queryResult.outputContexts[0].parameters['first-name'], request.body.queryResult.outputContexts[0].parameters['last-name'], '%' + request.body.queryResult.outputContexts[0].parameters.Course + '%'],
+                nextQuestionContext: "get-date-of-birth"
+            }
+        default:
+            return {
 
+            }
+    }
+}
 
 var apply = function(request, intent, intentClassification) {
-    var queryString = intentClassification['queryString'];
-    var nextQuestionToAsk = intentClassification['nextQuestionContext'];
+    var appStageInfo = applicationStage(intent, request);
+    var queryString = appStageInfo['queryString'];
+    var queryParams = appStageInfo['queryParams'];
+    var nextQuestionToAsk = appStageInfo['nextQuestionContext'];
 
     var session = request.body.session;
-    var Course = request.body.queryResult.outputContexts[0].parameters.Course;
     var currentContext = request.body.queryResult.outputContexts[0];
-    var firstName = request.body.queryResult.outputContexts[0].parameters['first-name'];
-    var lastName = request.body.queryResult.outputContexts[0].parameters['last-name'];
 
     return new Promise(function(resolve, reject){
-        db.query(queryString, [firstName, lastName, '%' + Course + '%'])
+        db.query(queryString, queryParams)
             .then(result => {
                 var id = result.rows[0]['student_id'];
+                var fulfilText;
                 resolve({
                     fulfillmentText: "You have successfully started your application.",
                     outputContexts: [{
