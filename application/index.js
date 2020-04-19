40b424b646bbbf6e5c4e622f4deda49c8e08302a
Sailesh Patel <saileshapatel@gmail.com>
2020-04-19 02:44:42 +0100

    add validation check

--- application/index.js
+++ application/index.js
@@ -5,10 +5,21 @@ function applicationStage(stage, request){
 
     switch(stage){
         case 'Application - yes':
-            return {
-                queryString: "INSERT INTO students (first_name, last_name, ucas_code) VALUES ($1, $2, (SELECT ucas_code FROM courses WHERE course_name LIKE $3)) RETURNING student_id;",
-                queryParams: [context.parameters['first-name'], context.parameters['last-name'], '%' + context.parameters.Course + '%'],
-                nextQuestionContext: "get-date-of-birth"
+            var firstName = context.parameters['first-name'];
+            var lastName = context.parameters['last-name'];
+
+            if(firstName.length <= 0 || lastName.length <= 0){
+                return {
+                    valid: false
+                }
+            } else {
+
+                return {
+                    queryString: "INSERT INTO students (first_name, last_name, ucas_code) VALUES ($1, $2, (SELECT ucas_code FROM courses WHERE course_name LIKE $3)) RETURNING student_id;",
+                    queryParams: [context.parameters['first-name'], context.parameters['last-name'], '%' + context.parameters.Course + '%'],
+                    nextQuestionContext: "get-date-of-birth",
+                    valid: true
+                }
             }
         default:
             return {
@@ -27,29 +38,37 @@ var apply = function(request, intent) {
     var currentContext = request.body.queryResult.outputContexts[0];
 
     return new Promise(function(resolve, reject){
-        db.query(queryString, queryParams)
-            .then(result => {
-                var id = result.rows[0]['student_id'];
-                resolve({
-                    fulfillmentText: "You have successfully started your application.",
-                    outputContexts: [{
-                        "name": session + "/contexts/" + nextQuestionToAsk,
-                        "lifespanCount": 1,
-                        "parameters": {
-                            "student_id": id,
-                        }
-                    }],
-                    source: 'getcourse'                    
+        if(appStageInfo['valid']){
+            db.query(queryString, queryParams)
+                .then(result => {
+                    var id = result.rows[0]['student_id'];
+                    resolve({
+                        fulfillmentText: "You have successfully started your application.",
+                        outputContexts: [{
+                            "name": session + "/contexts/" + nextQuestionToAsk,
+                            "lifespanCount": 1,
+                            "parameters": {
+                                "student_id": id,
+                            }
+                        }],
+                        source: 'getcourse'                    
+                    })
                 })
-            })
-            .catch(err => {
-                console.log(err);
-                reject({
-                    fulfillmentText: "There was an error and your application has not been started at this time. Please try again.",
-                    outputContexts: [currentContext],
-                    source: 'getcourse'
+                .catch(err => {
+                    console.log(err);
+                    reject({
+                        fulfillmentText: "There was an error and your application has not been started at this time. Please try again.",
+                        outputContexts: [currentContext],
+                        source: 'getcourse'
+                    })
                 })
+        } else {
+            reject({
+                fulfillmentText: "Your first or last name is blank. Please enter a valid first or last name.",
+                outputContexts: [currentContext],
+                source: 'getcourse'
             })
+        }
     })
 }
 
