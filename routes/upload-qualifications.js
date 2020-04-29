07975c50031df2f3e53f34437e2127c8796c1a10
Sailesh Patel <saileshapatel@gmail.com>
2020-04-29 21:46:13 +0100

    add upload qualification view

--- routes/upload-qualifications.js
+++ routes/upload-qualifications.js
@@ -2,3 +2,6 @@ const db = require("./../db");
 var express = require("express");
 var router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('upload-qualifications', {title: 'Upload Qualifications'});
+});
