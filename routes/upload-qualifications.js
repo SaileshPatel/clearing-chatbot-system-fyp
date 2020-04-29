7ffa9658ceae2384a23e4b925f3d6c82f665fe78
Sailesh Patel <saileshapatel@gmail.com>
2020-04-29 22:17:27 +0100

    create function to retrieve all student IDs

--- routes/upload-qualifications.js
+++ routes/upload-qualifications.js
@@ -8,4 +8,17 @@ router.get('/', (req, res) => {
     res.render('upload-qualifications', {title: 'Upload Qualifications', student_id: student_id, id_present: id_exists});
 });
 
+
+function getAllStudentIDs(){
+    return new Promise(function(resolve, reject){
+        db.query("SELECT student_id FROM students ORDER BY student_id ASC;", [])
+            .then(response => {
+                resolve(response.rows);
+            })
+            .catch(err => {
+                reject("error: we were unable to retrieve values")
+            })
+    })
+}
+
 module.exports = router;
