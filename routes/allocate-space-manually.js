9e0194da2e425bbb4ebf9bc0dd36e575af9a16a8
Sailesh Patel <saileshapatel@gmail.com>
2020-05-03 14:05:51 +0100

    add course deletion feature

--- routes/allocate-space-manually.js
+++ routes/allocate-space-manually.js
@@ -13,22 +13,45 @@ router.get('/', (req, res) => {
 })
 
 router.post("/", (req, res) => {
-    var course_to_update = req['body']['ucas_code'];
+    var course = req['body']['ucas_code'];
 
-    allocateSpace(course_to_update)
-        .then(response => {
-            getAllCourses()
+    if(req['body']['allocate'] != null){
+        allocateSpace(course)
+            .then(response => {
+                getAllCourses()
+                    .then(resp => {
+                        res.render('add-space-manually', {title: "Allocate Space", courses: resp});
+                    })
+                    .catch(err => {
+                        res.render('add-space-manually', {title: "Allocate Space", fail_message: err});
+                    })
+                })
+            .catch(err => {
+                res.render('add-space-manually', {title: "Allocate Space", fail_message: err['fail_message']});
+            })
+    } else if (req['body']['delete'] != null) {
+        deleteSpace(course)
+            .then(response => {
+                getAllCourses()
                 .then(resp => {
-                    res.render('add-space-manually', {title: "Allocate Space", courses: resp});
+                        res.render('add-space-manually', {title: "Allocate Space", courses: resp});
+                    })
+                    .catch(err => {
+                        res.render('add-space-manually', {title: "Allocate Space", fail_message: err});
+                    })
                 })
                 .catch(err => {
-                    res.render('add-space-manually', {title: "Allocate Space", fail_message: err});
+                    res.render('add-space-manually', {title: "Allocate Space", fail_message: err['fail_message']});
                 })
-        })
-        .catch(err => {
-            res.render('add-space-manually', {title: "Allocate Space", fail_message: err['fail_message']});
-
-        })
+    } else {
+        getAllCourses()
+            .then(response => {
+                res.render('add-space-manually', {title: "Allocate Space", courses: response});
+            })
+            .catch(err => {
+                res.render('add-space-manually', {title: "Allocate Space", fail_message: err});
+            })
+    }
 })
 
 function getAllCourses(){
@@ -53,6 +76,20 @@ function allocateSpace(ucas_code){
             .catch(err => {
                 reject({
                     fail_message: 'There has been an issue with adding a space to the course at this time'
+                })
+            })
+    })
+}
+
+function deleteSpace(ucas_code){
+    return new Promise(function(resolve, reject){
+        db.query("DELETE FROM Courses WHERE ucas_code = $1", [ucas_code])
+            .then(response => {
+                resolve()
+            })
+            .catch(err => {
+                reject({
+                    fail_message: 'There has been an issue with deleting a course at this time'
                 })
             })
     })
